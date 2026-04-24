05327c9a8d74e294643aa779c2c9273b23ae5279
stevencardenas <kevinmarin1012@gmail.com>
2026-04-23 23:44:50 -0500

    JWT y CAPTCHA

---
+++ backend/src/main/java/com/gestionganadera/backend/dto/RecaptchaResponse.java
@@ -0,0 +1,14 @@
+package com.gestionganadera.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class RecaptchaResponse {
+    private boolean success;
+    private String challenge_ts;
+    private String hostname;
+    @JsonProperty("error-codes")
+    private List<String> errorCodes;
+}
